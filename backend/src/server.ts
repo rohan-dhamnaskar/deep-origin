44dd7b85f4a76e000e395c16f5ca2a5f198b8d3a
Rohan Dhamnaskar <rohan@kiwico.com>
2025-05-22 18:28:31 -0700

    feat: Docker startup + hot-reloading + ESLint + Prettier

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,27 +1,27 @@
-import Hapi from '@hapi/hapi';
-import { Request, ResponseToolkit } from '@hapi/hapi';
+import Hapi from "@hapi/hapi";
+import { Request, ResponseToolkit } from "@hapi/hapi";
 
 const init = async () => {
-    const server = Hapi.server({
-        port: 3000,
-        host: '0.0.0.0'
-    });
+  const server = Hapi.server({
+    port: 3000,
+    host: "0.0.0.0",
+  });
 
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: (request: Request, h: ResponseToolkit) => {
-            return "Welcome to the URL Shortener API!";
-        }
-    });
+  server.route({
+    method: "GET",
+    path: "/",
+    handler: (request: Request, h: ResponseToolkit) => {
+      return "Welcome to the URL Shortener API!";
+    },
+  });
 
-    await server.start();
-    console.log('Server running on %s', server.info.uri);
+  await server.start();
+  console.log("Server running on %s", server.info.uri);
 };
 
-process.on('unhandledRejection', (err: Error) => {
-    console.log(err);
-    process.exit(1);
+process.on("unhandledRejection", (err: Error) => {
+  console.log(err);
+  process.exit(1);
 });
 
 init();
