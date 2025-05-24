1658e6f7df7aee4ab9cfb2508880e602e60beb50
Rohan Dhamnaskar <rohan@kiwico.com>
2025-05-23 17:44:25 -0700

    feat: front-end initial commit

---
+++ frontend/next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  experimental: {
+    // Any experimental features you want to enable
+  },
+  serverOptions: {
+    port: 3001,
+  },
+};
+
+export default nextConfig;
